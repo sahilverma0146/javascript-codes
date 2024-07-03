0fc746be5d5e7bc1facf5c05b22ad9bf8a284cbe
Sahil <sahilverma22146@gmail.com>
2024-07-03 15:26:44 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,92 @@
+// console.log("apna college")
+// console.log("apna college")
+// console.log("apna college")
+// console.log("apna college")
+// console.log("apna college")
+// console.log("apna college")
+
+//  for loop
+
+// let b=5;
+// for( let  i=1; i<b; i++){
+//     console.log("hello world");
+// }
+
+
+// var  sum=0;
+// for( let i=1; i<5; i++){    //i is defined only in the block for let
+//     console.log("i :" ,i);
+//     sum = sum + i; // sum =0 +1; sum =1;
+// }
+// console.log(sum);
+// console.log("i :" ,i); // it shows i is undefined
+
+
+
+// var  sum=1;
+// for( var i=1; i<=5; i++){    //i is defined globally only in the block by var
+//     sum = sum + i; // sum =0 +1; sum =1;
+//     console.log("i :" ,i);
+// }
+// console.log(sum);
+// console.log("i :" ,i); // shows i defined n assign value 6
+
+// infinite loops -- never do in the program . it is a never ending loops hence it takes the whole memory
+
+// while loops 
+// var i =2;
+// while( i<22){
+//     console.log("i:", i);
+//     i++;   // muust h updation 
+// }
+
+// do-while loops
+
+// let i=1;
+// do{
+//     console.log("i:",i );
+//     i++;
+// }while(i<=10);
+
+//  for-of loop --- for-of --- to apply loops in array or strings
+//              --- for-in --- used for objects( this loop  return the keys) n arrays
+
+// let strings = "sahil verma";
+// let size =0;
+// for( let i of strings){
+//     console.log("i:", i);
+//     size++;
+
+// }
+// console.log("string size :", size);
+
+
+//  for-in loops
+
+// making a object
+
+// let student = {
+//     name : "sahil verma", // name  rollno age cgpa are keys
+//     roll_no : 4570,
+//     age : 22,
+//     cgpa :7.2
+// };
+
+// for( let key in student){
+//     console.log( "keys :",key , "value :", student[key] );
+// }
+
+
+//  print even no from 0 to 100
+
+for( let i=0; i<=100; i++){
+    if(i%2==0){
+        console.log(i);
+        
+    }
+    // console.log( "numbers : ",i); // to print all numbers 
+}
+
+
+
+
