e5c2049b19701c09612f35c594073b2ac57c31fa
Sahil <sahilverma22146@gmail.com>
2024-07-06 19:44:25 +0530

    Create functionnmethod.js

---
+++ functionnmethod.js
@@ -0,0 +1,51 @@
+// // function def
+// function students(){  // parameter --- input --- local variables
+//     for(let i=0; i<5; i++ ){
+//         console.log("hello mr sahil ");
+//     }
+// }
+// // function calling / invoking
+// students(); // argument
+
+// prob 
+
+// function sum(x,y){  // x,y re local variables
+//     sum = x+y;
+//     console.log(sum);
+
+// }
+// sum(3,4);
+
+
+//   arrow functions  -- concept of modern js
+
+// const result =(x,y)=>{
+//     sum =x+y;
+//     console.log(sum);
+   
+// }
+// result(4,5);
+
+// let multipy=(x,y)=>{
+//     mul = x*y;
+//     console.log(mul);
+// }
+// multipy(5,4);
+
+
+// prob --- take string as a input and check how many chr of string are vowels
+// var count =0;
+// function vowel(string){
+//     for(let chr of string){
+//         console.log(chr);
+//         if(chr ==="a" ||chr ==="e" ||chr ==="i" ||chr ==="o" ||chr ==="u"  ){
+//             count++;
+//         }
+       
+//     }
+//     console.log(count);
+
+// }
+// vowel("aeiou");
+
+
