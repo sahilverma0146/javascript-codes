c25b8eaae3a6091ec6265885a10cd7c17e1e80f5
Sahil <sahilverma22146@gmail.com>
2024-07-09 12:52:00 +0530

    Update dom.js

---
+++ dom.js
@@ -0,0 +1,38 @@
+// get element by id
+
+// let button1=document.getElementById("topbutton");
+// console.log(button1);
+
+
+// get element by clss
+// let headings =document.getElementsByClassName("heading-cls");
+// console.log(headings);
+
+// elements by tags
+
+// let paragraphs=document.getElementsByTagName("p");
+// console.log(paragraphs);
+
+
+// alt of get elements --- it gives only the firest element
+// let elements = document.querySelector("p");
+// console.log(elements);
+
+
+// let newEl = document.querySelectorAll("p");
+// console.log(newEl);
+
+
+// let newEl = document.querySelectorAll(".heading-cls");
+// console.log(newEl);
+
+// let newEl = document.querySelectorAll("#id");
+// console.dir(newEl);
+// console.log(newEl);
+
+
+//  properties
+
+// tagname
+// console.log(topbutton.tagname);
+// console.log(p.tagname);
