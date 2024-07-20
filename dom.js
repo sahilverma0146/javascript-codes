90212b70cf24e60079342bb2839e8b47fef0798e
Sahil <sahilverma22146@gmail.com>
2024-07-20 17:50:06 +0530

    Update dom.js

--- dom.js
+++ dom.js
@@ -36,3 +36,29 @@
 // tagname
 // console.log(topbutton.tagname);
 // console.log(p.tagname);
+
+// p.innertext --- only gives the inner text of the element
+// p.innerhtml --- gives the inner text with its tags names
+
+// let heading = document.querySelector("h1");
+// console.log(heading);
+// console.log(heading.innerText="newhead");
+
+// textcontent --- shows the hiiden elements in the div
+
+//  questions 
+
+//  chnge "hello js" to from apna college students
+
+// let element = document.querySelector("h2");
+// console.log(element);
+// console.log(element.innerText);
+// console.log(element.innerText="from appna college students");
+
+
+// problem 2 --- question of 2 divs
+
+// let box = document.querySelector(".boxes");
+// // console.log(box);
+// let div = box[2].innertext="new box one";
+// console.log(div);
